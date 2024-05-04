6126827233a0cd8b4ea214ce93431efd3e506588
ManuelFBS <manuelf.borrego@gmail.com>
2024-05-04 06:59:57 -0400

    Modificación en mailer.ts

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -12,14 +12,6 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
-// const transporter = nodemailer.createTransport({
-//   service: 'Gmail',
-//   auth: {
-//     user: 'mfbsmail.fortesting@gmail.com',
-//     pass: process.env.GKEY,
-//   },
-// });
-
 transporter.verify().then(() => {
   console.log('Ready for send emails');
 });
