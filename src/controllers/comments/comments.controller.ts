1103dd094da340381166792f3939b8ea5e67f352
ManuelFBS <manuelf.borrego@gmail.com>
2024-05-06 07:02:35 -0400

    Creación del método "deleteComment" (comments.controller.ts)

--- src/controllers/comments/comments.controller.ts
+++ src/controllers/comments/comments.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { User } from '../../entities/User';
 import { Article } from '../../entities/Article';
 import { Comment } from '../../entities/Comment';
+import { isEmailType } from '../../libs/vartype';
 
 export const createNewComment = async (
   req: Request,
@@ -51,4 +52,53 @@ export const createNewComment = async (
       return res.status(500).json(error);
     }
   }
+};
+
+export const deleteComment = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const {
+      email,
+      userName,
+      articleID,
+      commentID,
+    }: {
+      email: string;
+      userName: string;
+      articleID: number;
+      commentID: number;
+    } = req.body;
+
+    let query: object = {};
+
+    if (isEmailType(email)) {
+      query = { where: { email: email } };
+    } else {
+      query = { where: { userName: userName } };
+    }
+
+    const user = await User.findOne(query);
+
+    if (!user)
+      return res
+        .status(404)
+        .json({ message: 'User not found...!' });
+
+    if (req.userRole !== 'owner')
+      return res.status(401).json({
+        message:
+          'You are not authorized to perform this operation...!',
+      });
+
+    const article = await Article.findOne({
+      where: { id: articleID },
+    });
+
+    if (!article)
+      return res
+        .status(404)
+        .json({ message: 'Article not found...!' });
+  } catch (error) {}
 };
