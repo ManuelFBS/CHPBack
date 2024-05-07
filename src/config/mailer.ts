e4a520c80e001a7071b2f9f6ed05c400f2f16d2a
ManuelFBS <manuelf.borrego@gmail.com>
2024-05-07 11:40:02 -0400

    Modificación en mailer.ts

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-export const emailConfig = {
-  host: 'smtp.gmail.com', // Cambia esto al servidor SMTP que estés utilizando
-  port: 587, // Cambia esto al puerto SMTP correspondiente
-  secure: false,
-  requireTLS: true,
-  user: 'oteroalan06@gmail.com', // Cambia esto a tu dirección de correo electrónico
-  password: process.env.GKEY, // Cambia esto a tu contraseña
-};
+// export const emailConfig = {
+//   host: 'smtp.gmail.com', // Cambia esto al servidor SMTP que estés utilizando
+//   port: 587, // Cambia esto al puerto SMTP correspondiente
+//   secure: false,
+//   requireTLS: true,
+//   user: 'manuelf.borrego@gmail.com', // Cambia esto a tu dirección de correo electrónico
+//   password: process.env.GKEY, // Cambia esto a tu contraseña
+// };
 
 // import nodemailer from 'nodemailer';
 // import dotenv from 'dotenv';
